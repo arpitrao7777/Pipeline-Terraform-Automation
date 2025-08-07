572f3bf498eebd669ffbe6d34fc9c3eeaa111a92
Arpit <arpit32971@gmail.com>
2025-08-07 15:39:14 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -9,16 +9,17 @@ terraform {
       version = "3.7.2"
     }
   }
-    # backend "azurerm" {
-    # resource_group_name  = "rg-001"
-    # storage_account_name = "test8307532971"
-    # container_name       = "developement"
-    # key                  = "main.terraform.tfstate"
-    # }
+     backend "azurerm" {
+     resource_group_name  = "rg-001"
+     storage_account_name = "test8307532971"
+     container_name       = "developement"
+     key                  = "main.terraform.tfstate"
+     }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "5fffa1ff-217c-45ea-ad98-83726d1375b9"
 }
+
 
